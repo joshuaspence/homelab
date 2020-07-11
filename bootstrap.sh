349ee0056a3241fa46267b3b75fb80f84cc01800
Joshua Spence <josh@joshuaspence.com>
2020-07-11 19:33:26 +1000

    Tweak bootstrap again

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,12 +9,5 @@ helm repo add fluxcd https://charts.fluxcd.io
 
 # Bootstrap Flux and Helm Operator.
 kubectl apply --filename flux/namespace.yaml
-helm install --namespace flux --set git.readonly=true --set git.url=https://github.com/joshuaspence/homelab.git flux fluxcd/flux
-helm install --namespace flux --set helm.versions=v3 helm-operator fluxcd/helm-operator
-
-# --atomic
-# --no-hooks
-# --repo
-# --verify
-# --wait
-
+helm install --atomic --namespace flux --set git.readonly=true --set git.url=https://github.com/joshuaspence/homelab.git flux fluxcd/flux
+helm install --atomic --namespace flux --set helm.versions=v3 helm-operator fluxcd/helm-operator
