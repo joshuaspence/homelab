0cc19ffc1bd9ad76cb4be14e48cd2a2c1fe1313e
Joshua Spence <josh@joshuaspence.com>
2020-07-12 12:54:08 +1000

    Improve bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,4 +4,11 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-fluxctl install --git-email support@weave.works --git-readonly --git-url https://github.com/joshuaspence/homelab.git --namespace flux | kubectl --filename -
+# Bootstrap Flux.
+kubectl create namespace flux
+fluxctl install --git-email support@weave.works --git-url $(gh api repos/:owner/:repo | jq --raw-output .ssh_url) --namespace flux | kubectl apply --filename -
+kubectl --namespace flux rollout status deployment/flux
+
+# Add SSH key to repo.
+gh api repos/:owner/:repo/keys | jq '.[] | .id' | xargs --replace gh api repos/:owner/:repo/keys/{} --method DELETE
+fluxctl --k8s-fwd-ns flux identity | gh api repos/:owner/:repo/keys --field 'title=Flux' --field 'key=@-' --field 'read_only=false'
