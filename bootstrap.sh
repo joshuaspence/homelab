9d8bc534152262ddb7ded1e1a430d703cad56e5a
Joshua Spence <josh@joshuaspence.com>
2020-07-15 22:07:05 +1000

    Create a `FLUX_KEY` variable

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,17 +7,19 @@ set -o pipefail
 # Print commands as they are executed.
 trap 'echo "# $BASH_COMMAND"' DEBUG
 
-# Create SSH key and upload to GitHub.
-if ! test -f ~/.ssh/keys/flux; then
-  ssh-keygen -C flux -f ~/.ssh/keys/flux -N '' -q -t ed25519
+# Create deployment key and upload to GitHub.
+readonly FLUX_KEY="${HOME}/.ssh/keys/flux"
+
+if ! test -f "${FLUX_KEY}"; then
+  ssh-keygen -C flux -f "${FLUX_KEY}" -N '' -q -t ed25519
 
   gh api repos/:owner/:repo/keys | jq '.[] | .id' | xargs --replace gh api repos/:owner/:repo/keys/{} --method DELETE
-  gh api repos/:owner/:repo/keys --field 'title=Flux' --field "key=@${HOME}/.ssh/keys/flux.pub" --field 'read_only=false' >/dev/null
+  gh api repos/:owner/:repo/keys --field 'title=Flux' --field "key=@${FLUX_KEY}.pub" --field 'read_only=false' >/dev/null
 fi
 
 # Bootstrap Flux (see https://docs.fluxcd.io/en/1.18.0/tutorials/get-started-helm.html).
 kubectl create namespace flux
-kubectl --namespace flux create secret generic flux-git-deploy --from-file "identity=${HOME}/.ssh/keys/flux"
+kubectl --namespace flux create secret generic flux-git-deploy --from-file "identity=${FLUX_KEY}"
 helm install --namespace flux --repo https://charts.fluxcd.io --set git.path=deployments --set git.secretName=flux-git-deploy --set git.url=$(gh api repos/:owner/:repo | jq --raw-output .ssh_url) --wait flux flux >/dev/null
 helm install --namespace flux --repo https://charts.fluxcd.io --set helm.versions=v3 helm-operator --wait helm-operator >/dev/null
 
