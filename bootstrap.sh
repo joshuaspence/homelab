13feaa81164eb41a7446bdd7e0310b8f2b4d18b1
Joshua Spence <josh@joshuaspence.com>
2020-07-15 18:53:21 +1000

    Pass `--repo` to `helm install`

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,20 +10,22 @@ export FLUX_FORWARD_NAMESPACE=flux
 # Print commands as they are executed.
 trap 'echo "# $BASH_COMMAND"' DEBUG
 
-# Add the Flux repository.
-helm repo add fluxcd https://charts.fluxcd.io
-
 # Apply the `HelmRelease` CRD.
 kubectl apply --filename https://raw.githubusercontent.com/fluxcd/helm-operator/master/deploy/crds.yaml
 
+# Create SSH key and upload to GitHub.
+if ! test -f ~/.ssh/keys/flux; then
+  ssh-keygen -C flux -f ~/.ssh/keys/flux -N '' -q -t ed25519
+
+  gh api repos/:owner/:repo/keys | jq '.[] | .id' | xargs --replace gh api repos/:owner/:repo/keys/{} --method DELETE
+  gh api repos/:owner/:repo/keys --field 'title=Flux' --field "key=@${HOME}/.ssh/keys/flux.pub" --field 'read_only=false' >/dev/null
+fi
+
 # Bootstrap Flux (see https://docs.fluxcd.io/en/1.18.0/tutorials/get-started-helm.html).
 kubectl create namespace flux
-helm install --namespace flux --set git.path=deployments --set git.url=$(gh api repos/:owner/:repo | jq --raw-output .ssh_url) --wait flux fluxcd/flux >/dev/null
-helm install --namespace flux --set helm.versions=v3 helm-operator --wait fluxcd/helm-operator >/dev/null
-
-# Add SSH key to repo.
-gh api repos/:owner/:repo/keys | jq '.[] | .id' | xargs --replace gh api repos/:owner/:repo/keys/{} --method DELETE
-fluxctl identity | gh api repos/:owner/:repo/keys --field 'title=Flux' --field 'key=@-' --field 'read_only=false' >/dev/null
+kubectl --namespace flux create secret generic flux-git-deploy --from-file "identity=${HOME}/.ssh/keys/flux"
+helm install --namespace flux --repo https://charts.fluxcd.io --set git.path=deployments --set git.secretName=flux-git-deploy --set git.url=$(gh api repos/:owner/:repo | jq --raw-output .ssh_url) --wait flux flux >/dev/null
+helm install --namespace flux --repo https://charts.fluxcd.io --set helm.versions=v3 helm-operator --wait helm-operator >/dev/null
 
 # Force a sync.
-retry --times 30 -- fluxctl sync
+fluxctl sync
