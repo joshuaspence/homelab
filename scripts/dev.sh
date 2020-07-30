7faf830321978a0e495cd826c86059ff8e5e448d
Joshua Spence <josh@joshuaspence.com>
2020-07-30 14:06:22 +1000

    Minor

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -7,8 +7,8 @@ set -o pipefail
 # Create Kubernetes cluster.
 cat <<EOF | kind create cluster --config=-
 ---
-kind: 'Cluster'
 apiVersion: 'kind.x-k8s.io/v1alpha4'
+kind: 'Cluster'
 nodes:
   - role: 'control-plane'
   - role: 'worker'
