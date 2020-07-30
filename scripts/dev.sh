fbc2860d15edb7c72045102d1676bc446dc8bd04
Joshua Spence <josh@joshuaspence.com>
2020-07-30 13:56:43 +1000

    Remove `extraPortMappings`

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -11,13 +11,6 @@ kind: 'Cluster'
 apiVersion: 'kind.x-k8s.io/v1alpha4'
 nodes:
   - role: 'control-plane'
-    extraPortMappings:
-      - containerPort: 80
-        hostPort: 80
-        protocol: 'TCP'
-      - containerPort: 443
-        hostPort: 443
-        protocol: 'TCP'
 EOF
 
 # Bootstrap Flux.
