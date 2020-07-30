4f20257ae1cca8fc9a27bd3c1a66cc36ce3b54b0
Joshua Spence <josh@joshuaspence.com>
2020-07-30 15:08:29 +1000

    Add `metallb`

---
+++ scripts/dev.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# Create Kubernetes cluster.
+kind create cluster --config kind.yaml
+
+# Bootstrap Flux.
+./scripts/bootstrap.sh
+
+# Add routes for MetalLB.
+for ADDRESS in $(yq read src/kube-system/metallb.yaml spec.values.configInline.address-pools[*].addresses[*]); do
+  sudo ip route replace "${ADDRESS}" dev "br-$(docker network ls --filter name=kind --quiet)"
+done
