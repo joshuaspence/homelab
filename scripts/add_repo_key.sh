31384cde549080f0133e4fa903ec532a8873b93d
Joshua Spence <josh@joshuaspence.com>
2020-08-06 23:18:47 +1000

    Add script to add deployment key

---
+++ scripts/add_repo_key.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# Delete existing deployment keys.
+gh api repos/:owner/:repo/keys | jq '.[] | .id' | xargs --replace gh api repos/:owner/:repo/keys/{} --method DELETE
+
+# Create a new deployment key.
+gh api repos/:owner/:repo/keys --field 'title=Flux' --field key=@<(sops --decrypt src/flux/secrets.yaml | yq read --doc '*' --collect - | yq read - '(metadata.name==flux-git).data.identity' | base64 --decode | ssh-keygen -f /dev/stdin -y) --field 'read_only=false' >/dev/null
