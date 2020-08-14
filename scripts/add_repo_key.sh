88fa513b13d0a4e374b513a07e2c2b695b8c761e
Joshua Spence <josh@joshuaspence.com>
2020-08-14 22:33:14 +1000

    Tweak script

--- scripts/add_repo_key.sh
+++ scripts/add_repo_key.sh
@@ -4,8 +4,13 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-# Delete existing deployment keys.
-gh api repos/:owner/:repo/keys | jq '.[] | .id' | xargs --replace gh api repos/:owner/:repo/keys/{} --method DELETE
+# Delete existing deployment key.
+gh api repos/:owner/:repo/keys | jq '.[] | select(.title == "Flux") | .id' | xargs --replace gh api --method DELETE repos/:owner/:repo/keys/{}
 
 # Create a new deployment key.
-gh api repos/:owner/:repo/keys --field 'title=Flux' --field key=@<(sops --decrypt src/flux/secrets.sops.yaml | yq read --doc '*' --collect - | yq read - '(metadata.name==flux-git).data.identity' | base64 --decode | ssh-keygen -f /dev/stdin -y) --field 'read_only=false' >/dev/null
+gh api \
+  --field 'title=Flux' \
+  --field key=@<(sops --decrypt src/flux/secrets.sops.yaml | yq read --doc '*' --collect - | yq read - '(metadata.name==flux-git).data.identity' | base64 --decode | ssh-keygen -f /dev/stdin -y) \
+  --field 'read_only=false' \
+  --silent \
+  repos/:owner/:repo/keys
