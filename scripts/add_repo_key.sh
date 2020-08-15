d9e169c8d89bc6ef3b7722c17b2019a8508c71fa
Joshua Spence <josh@joshuaspence.com>
2020-08-15 18:54:19 +1000

    Improve readability of `scripts/add_repo_key.sh`

--- scripts/add_repo_key.sh
+++ scripts/add_repo_key.sh
@@ -5,12 +5,20 @@ set -o nounset
 set -o pipefail
 
 # Delete existing deployment key.
-gh api repos/:owner/:repo/keys | jq '.[] | select(.title == "Flux") | .id' | xargs --replace gh api --method DELETE repos/:owner/:repo/keys/{}
+gh api repos/:owner/:repo/keys |
+  jq '.[] | select(.title == "Flux") | .id' |
+  xargs --replace gh api --method DELETE repos/:owner/:repo/keys/{}
 
 # Create a new deployment key.
 gh api \
   --field 'title=Flux' \
-  --field key=@<(sops --decrypt src/flux/secrets.sops.yaml | yq read --doc '*' --collect - | yq read - '(metadata.name==flux-git).data.identity' | base64 --decode | ssh-keygen -f /dev/stdin -y) \
+  --field key=@<(
+    sops --decrypt src/flux/secrets.sops.yaml |
+      yq read --doc '*' --collect - |
+      yq read - '(metadata.name==flux-git).data.identity' |
+      base64 --decode |
+      ssh-keygen -f /dev/stdin -y
+  ) \
   --field 'read_only=false' \
   --silent \
   repos/:owner/:repo/keys
