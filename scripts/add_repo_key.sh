fde0ac99db870e657f3078d296b3a7bf79c04c03
Joshua Spence <josh@joshuaspence.com>
2020-08-17 12:48:16 +1000

    Tweak script slightly

--- scripts/add_repo_key.sh
+++ scripts/add_repo_key.sh
@@ -4,16 +4,17 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-readonly PRIVATE_KEY=$(
+readonly PUBLIC_KEY=$(
   sops --decrypt src/flux/secrets.sops.yaml |
     yq read --collect --doc '*' - |
     yq read - '(metadata.name==flux-git-deploy).data.identity' |
-    base64 --decode
+    base64 --decode |
+    ssh-keygen -f /dev/stdin -y
 )
-readonly PUBLIC_KEY=$(echo "${PRIVATE_KEY}" | ssh-keygen -f /dev/stdin -y)
 
-# Check for existing key.
-if gh api repos/:owner/:repo/keys | jq --exit-status --arg public_key "${PUBLIC_KEY}" '.[] | select(.key == $public_key)'; then
+# Check if deployment key already exists.
+if gh api repos/:owner/:repo/keys | jq --exit-status --arg public_key "${PUBLIC_KEY}" '.[] | select(.key == $public_key)' >/dev/null; then
+  echo 'Deployment key already exists.'
   exit 0
 fi
 
@@ -27,5 +28,4 @@ gh api \
   --field 'title=Flux' \
   --field "key=${PUBLIC_KEY}" \
   --field 'read_only=false' \
-  --silent \
   repos/:owner/:repo/keys
