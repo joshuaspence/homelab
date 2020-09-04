d06e6dedfcde5bcc293f3d90c5ad09262c442856
Joshua Spence <josh@joshuaspence.com>
2020-09-04 14:20:46 +1000

    Add a backup script
    See https://cert-manager.io/docs/tutorials/backup/.

---
+++ scripts/backup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+kubectl get --namespace cert-manager --output yaml secret "$(yq read "src/cert-manager/issuers.yaml" spec.acme.privateKeySecretRef.name)"
+kubectl get --all-namespaces --output yaml certificates,certificaterequests
