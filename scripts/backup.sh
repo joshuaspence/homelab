4b034c455a7e39a4ab319926dbc98653dbd1af1d
Joshua Spence <josh@joshuaspence.com>
2020-09-20 19:30:28 +1000

    Add a comment

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -4,5 +4,6 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+# See https://cert-manager.io/docs/tutorials/backup/.
 kubectl get --namespace cert-manager --output yaml secret "$(yq read "src/cert-manager/issuers.yaml" spec.acme.privateKeySecretRef.name)"
 kubectl get --all-namespaces --output yaml certificates,certificaterequests
